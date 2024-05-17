428468aadaee39631ec3c5c1584a4d3d07cfed1b
Joao Vieira <8200620@estg.ipp.pt>
2024-05-17 18:31:31 +0100

    Best Generation ViewPath

--- src/maps/PathDrawingSample.java
+++ src/maps/PathDrawingSample.java
@@ -15,9 +15,7 @@ public class PathDrawingSample {
     public static int bestIntersections = 0;
 
     public static void main(String args[]) throws InterruptedException, Exception {
-
-        int map_id = 9;
-
+        int map_id = 3;
         conf = Maps.getMap(map_id);
 
         int populationSize = 100;
@@ -27,10 +25,10 @@ public class PathDrawingSample {
         List<List<Point>> population = initializePopulation(populationSize, conf);
         List<Point> bestSolution = null;
         double bestFitness = Double.MAX_VALUE;
+        int bestGeneration = 0;
 
         for (int generation = 0; generation < generations; generation++) {
-            List<Double> fitnessValues = population.stream().map(path -> fitness(path, conf))
-                    .collect(Collectors.toList());
+            List<Double> fitnessValues = population.stream().map(path -> fitness(path, conf)).collect(Collectors.toList());
             List<List<Point>> selected = select(population, fitnessValues);
             List<List<Point>> offspring = crossover(selected);
             mutate(offspring, mutationRate, conf);
@@ -44,6 +42,7 @@ public class PathDrawingSample {
                     bestFitness = fitness;
                     bestSolution = currentPath;
                     bestIntersections = calculateIntersections(currentPath, conf);
+                    bestGeneration = generation;
                 }
             }
 
@@ -56,6 +55,7 @@ public class PathDrawingSample {
             System.out.println("Fitness: " + bestFitness);
             System.out.println("Intersections: " + bestIntersections);
             PathViewer pv = new PathViewer(conf);
+            pv.setGeneration(bestGeneration);
             pv.setFitness(bestFitness);
             pv.setStringPath(bestSolution.toString());
             List<IPoint> iPointSolution = bestSolution.stream().map(p -> new IPoint() {
@@ -85,7 +85,6 @@ public class PathDrawingSample {
         Random rand = new Random();
         for (int i = 0; i < populationSize; i++) {
             List<Point> path = new ArrayList<>();
-
             path.add(new Point(conf.getStart().getX(), conf.getStart().getY()));
             int size = rand.nextInt(5) + 1;
             for (int j = 0; j < size; j++) {
@@ -95,7 +94,6 @@ public class PathDrawingSample {
                 } while (isPointInObstacle(p, conf));
                 path.add(p);
             }
-
             path.add(new Point(conf.getEnd().getX(), conf.getEnd().getY()));
             population.add(path);
         }
@@ -128,7 +126,7 @@ public class PathDrawingSample {
             }
         }
 
-        return distance + intersections * 10000; 
+        return distance + intersections * 10000;
     }
 
     public static int calculateIntersections(List<Point> path, IUIConfiguration conf) {
@@ -137,7 +135,6 @@ public class PathDrawingSample {
         for (int i = 0; i < path.size() - 1; i++) {
             Point p1 = path.get(i);
             Point p2 = path.get(i + 1);
-
             Line2D.Double line = new Line2D.Double(p1, p2);
             for (Rectangle obstacle : conf.getObstacles()) {
                 if (obstacle.intersectsLine(line)) {
@@ -145,7 +142,6 @@ public class PathDrawingSample {
                 }
             }
         }
-
         return intersections;
     }
 
--- src/viewer/PathViewer.java
+++ src/viewer/PathViewer.java
@@ -13,11 +13,11 @@ import interf.IUIConfiguration;
 /**
  * Class that shows a path in a GUI
  */
-public class PathViewer
-{
+public class PathViewer {
     private UI ui;
     private double fitness;
     private String stringPath;
+    private int generation;
     private IUIConfiguration conf;
 
     public PathViewer(IUIConfiguration conf) {
@@ -30,46 +30,30 @@ public class PathViewer
         this.conf = conf;
     }
 
-    /**
-     * Paints a path in the map. A path is a {@link List} of {@link IPoint}
-     * @param path the list of points to paint in the map
-     */
-    public void paintPath(List<IPoint> path){
+    public void paintPath(List<IPoint> path) {
         ui.paintPath(path);
     }
 
-    /**
-     * Updates the value of fitness to be shown in the map.
-     * @param fitness the value of fitness to be shown in the map
-     */
     public void setFitness(double fitness) {
         this.fitness = fitness;
     }
 
-    /**
-     * Updates the string that describes the path being drawn in the map
-     * @param stringPath the string that desceibes thepath being drawn
-     */
     public void setStringPath(String stringPath) {
         this.stringPath = stringPath;
     }
 
-    private class UI extends JPanel
-    {
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    private class UI extends JPanel {
         private final Color COR_CAMINHO = Color.GRAY;
         private final Color COR_LETRAS = Color.BLACK;
-        private final Color COR_PROCESSAMENTO = Color.green;
-        private final Color COR_SILO = Color.BLACK;
         private int largura, altura;
         private List<IPoint> path;
         private List<Rectangle> obstacles;
-        private int iteration = 0;
 
-        /**
-         * Creates new form FormigueiroGUI
-         */
-        private UI(int largura, int altura, List<Rectangle> obstacles)
-        {
+        private UI(int largura, int altura, List<Rectangle> obstacles) {
             initComponents();
             this.largura = largura;
             this.altura = altura;
@@ -80,57 +64,43 @@ public class PathViewer
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     super.mouseClicked(e);
-
-                    System.out.println("("+e.getX()+","+e.getY()+")");
+                    System.out.println("(" + e.getX() + "," + e.getY() + ")");
                 }
             });
         }
 
         private void paintPath(List<IPoint> caminho) {
             path = caminho;
-            iteration++;
             this.repaint();
         }
-
-        private void clearPath() {
-            this.path = new ArrayList<>();
-            this.repaint();
-        }
-
-
-
 
         @Override
         public void paintComponent(Graphics g) {
+            super.paintComponent(g);
             g.setColor(Color.white);
             g.fillRect(0, 0, largura, altura);
 
-            for (int i=1;i<path.size();i++)
-            {
-                drawThickLine(g, path.get(i-1).getX(), path.get(i-1).getY(), path.get(i).getX(), path.get(i).getY(), 2, COR_CAMINHO);
+            for (int i = 1; i < path.size(); i++) {
+                drawThickLine(g, path.get(i - 1).getX(), path.get(i - 1).getY(), path.get(i).getX(), path.get(i).getY(), 2, COR_CAMINHO);
             }
 
             g.setColor(Color.red);
-            obstacles.stream().forEach(x -> g.fillRect((int)x.getX(), (int)x.getY(), (int)x.getWidth(), (int)x.getHeight()));
+            obstacles.forEach(x -> g.fillRect((int) x.getX(), (int) x.getY(), (int) x.getWidth(), (int) x.getHeight()));
 
             g.setColor(Color.green);
             g.drawString("START", conf.getStart().getX(), conf.getStart().getY());
             g.drawString("END", conf.getEnd().getX(), conf.getEnd().getY());
 
-            g.setColor(Color.black);
-            g.drawString("Generation "+iteration+" Best Solution: "+stringPath+" ("+fitness+")", 20,20);
+            g.setColor(COR_LETRAS);
+            g.drawString("Generation: " + generation + " Best Solution: " + stringPath + " (Fitness: " + fitness + ")", 20, 20);
         }
 
         private void drawThickLine(Graphics g, int x1, int y1, int x2, int y2, int thickness, Color c) {
-
             g.setColor(c);
             int dX = x2 - x1;
             int dY = y2 - y1;
-
             double lineLength = Math.sqrt(dX * dX + dY * dY);
-
             double scale = (double) (thickness) / (2 * lineLength);
-
             double ddx = -scale * (double) dY;
             double ddy = scale * (double) dX;
             ddx += (ddx > 0) ? 0.5 : -0.5;
@@ -138,23 +108,12 @@ public class PathViewer
             int dx = (int) ddx;
             int dy = (int) ddy;
 
-            int xPoints[] = new int[4];
-            int yPoints[] = new int[4];
-
-            xPoints[0] = x1 + dx;
-            yPoints[0] = y1 + dy;
-            xPoints[1] = x1 - dx;
-            yPoints[1] = y1 - dy;
-            xPoints[2] = x2 - dx;
-            yPoints[2] = y2 - dy;
-            xPoints[3] = x2 + dx;
-            yPoints[3] = y2 + dy;
-
+            int xPoints[] = {x1 + dx, x1 - dx, x2 - dx, x2 + dx};
+            int yPoints[] = {y1 + dy, y1 - dy, y2 - dy, y2 + dy};
             g.fillPolygon(xPoints, yPoints, 4);
         }
 
-        private void initComponents()
-        {
+        private void initComponents() {
             GroupLayout layout = new GroupLayout(this);
             this.setLayout(layout);
             layout.setHorizontalGroup(
